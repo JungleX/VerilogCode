b6546948b99c89f6cbb3d77df5739630944aa23d
hull <hulili0203@126.com>
2017-07-17 14:45:56 +0800

    0

--- conv/lily/PU/PU.v
+++ conv/lily/PU/PU.v
@@ -2,10 +2,10 @@
 
 module PU 
 #(
-	
+	parameter integer PU_ID           = 0,
 	parameter integer OP_WIDTH        = 16,
 	
-	parameter integer NUM_PE          = 4
+	parameter integer NUM_PE          = 4,
 
 
 
@@ -21,7 +21,7 @@ module PU
 
 
 
-
+	parameter integer RD_LOOP_W         = 10
 
 
 
@@ -35,7 +35,7 @@ module PU
 	
 	
 	
-	
+	input wire                               bias_read_req,
 	
 	
 	
@@ -57,10 +57,10 @@ module PU
 	
 	
 	
+	input wire [ RD_LOOP_W      -1 : 0 ]     read_id;
 	
-	
-	
-	
+	input wire                               buffer_read_data_valid;
+	output wire                              read_req
 	
 	
 	
@@ -84,5 +84,141 @@ genvar i;
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+wire                                wb_weight_read_req;
+
+
+
+
+
+
+
+
+
+
+
+
+
+wire pu_bias_read_req;
+
+
+
+
+
+
+
+assign GND = 1024'd0;
+
+assign read_req = wb_weight_read_req || pu_bias_read_req;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+reg [ OP_WIDTH           -1 : 0 ]       bias;
+reg bias_v;
+
+
+wire weight_reset;
+assign weight_reset = bias_read_req;
+
+always @(posedge clk)
+	if (reset)
+		bias <= 0;
+	else if (pu_bias_read_req)
+		bias <= read_data[OP_WIDTH-1:0];
+
+always @(posedge clk)
+begin
+	if (reset)
+		bias_v <= 1'b0;
+	else if (weight_reset)
+		bias_v <= 1'b0;
+	else if (pu_bias_read_req)
+		bias_v <= 1'b1;
+end	
+
+assign pu_bias_read_req = buffer_read_data_valid && !bias_v && read_id == PU_ID;
+
+
+
+
+assign wb_weight_read_req = buffer_read_data_valid && bias_v && read_id == PU_ID;
+
+assign wb_write_req = wb_weight_read_req;
 
 endmodule
