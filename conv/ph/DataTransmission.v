b300a22f4aee3dbaffab7f633f11c9e2e5282f9c
Stan Pang <32975250+xShiva@users.noreply.github.com>
2017-11-25 21:39:09 +0800

    DT and FSM
    DT写好了，FSM还有几个接口没写

--- conv/ph/DataTransmission.v
+++ conv/ph/DataTransmission.v
@@ -47,29 +47,27 @@ module DataTransmission(
 reg [`PARA_X*`PARA_Y*`DATA_WIDTH - 1:0] fm_set_one;
 reg [`KERNEL_SIZE_MAX*`KERNEL_SIZE_MAX*`PARA_KERNEL*`DATA_WIDTH - 1:0] weight_set_one;
 
-generate
-genvar fm;
-for(fm = 0;fm < `PARA_X*`PARA_Y;fm = fm + 1) 
-begin:featuremap
-always @(*)
-fm_set_one[`DATA_WIDTH*(fm+1) - 1:`DATA_WIDTH*fm] = 16'h3c00;
+initial
+begin
+fm_set_one <= {`PARA_X*`PARA_Y{16'h3c00}};
+weight_set_one <= {`KERNEL_SIZE_MAX*`KERNEL_SIZE_MAX*`PARA_KERNEL{16'h3c00}};
 end
-endgenerate
-
-generate
-genvar wr;
-for(wr = 0;wr<`KERNEL_SIZE_MAX*`KERNEL_SIZE_MAX*`PARA_KERNEL;wr = wr + 1)
-begin:weightram
-always @(*)
-weight_set_one[`DATA_WIDTH*(fm+1) - 1:`DATA_WIDTH*fm] = 16'h3c00;
-end
-endgenerate
 //provide all-1 arrays for featuremap and weightram
 
 
 
 reg cnt;
+reg update_ena = 1'b0;
 always @(posedge clk) begin
+if (rst) begin
+init_fm_data <= 0;
+write_fm_data_addr <= 0;
+init_fm_data_done <= 0;
+weight_data <= 0;
+write_weight_data_addr <= 0;
+weight_data_done <= 0;
+update_ena <= 0;
+end
 if (init) begin
 init_fm_data <= fm_set_one;
 write_fm_data_addr <= 0;
@@ -84,13 +82,24 @@ end//signal "init" sustains for only 1 clock cycle.
 
 
 always @(posedge clk) if (cnt == 0) cnt <= 1; //delay for 1 clock cycle;
-always @(posedge clk) if ((cnt == 1) && (init_fm_ram_ready == 1) && (init_weight_ram_ready == 1)) init_fm_data_done <= 0; //if both these two "ready" signals are 1,set signal "done" into 0.
+always @(posedge clk) if ((cnt == 1) && (init_fm_ram_ready == 1)) init_fm_data_done <= 0;
+always @(posedge clk) if ((cnt == 1) && (init_weight_ram_ready == 1)) begin
+weight_data_done <= 0;
+update_ena <= 1;
+end
 
 
 
-//always @(posedge clk) begin
-//if (update_weight_ram) begin
-//weight_data <= {$random} % 2^`KERNEL_SIZE_MAX*`KERNEL_SIZE_MAX*`PARA_KERNEL*`DATA_WIDTH ;
+always @(posedge clk) begin
+if (update_weight_ram && update_ena) begin
+weight_data <= weight_set_one;
+write_weight_data_addr <= update_weight_ram_addr;
+weight_data_done <= 1;
+end
+end
 
-//connection between signal "update" and weightram which is not conpleted yet
+always @(posedge clk) begin
+if((!update_weight_ram) && update_ena) weight_data_done <= 0;
+end
+//connection between signal "update" and weightram 
 endmodule
--- conv/ph/FSM.v
+++ conv/ph/FSM.v
@@ -42,7 +42,12 @@ reg [`LAYER_NUM_WIDTH - 1:0] layer_num = `LAYER_NUM_WIDTH'b0;
 reg [1:0] layer_type; // 0: prepare init feature map and weight data; 1:conv; 2:pool; 3:fc;
 reg [1:0] pre_layer_type = 2'b00;
 
-always @(*) assign workstate = transmission_start;
+always @(*) assign workstate = transmission_start & (!rst);
+always @(posedge clk)
+if (rst) begin
+layer_num = `LAYER_NUM_WIDTH'b0;
+pre_layer_type = 2'b00;
+end
 
 reg init = 1'b0;
 reg dis_init = 1'b0;
@@ -64,6 +69,86 @@ default:layer_type <= 2'b00;
 endcase
 end
 end//define the layer type of each layer according to the structure of CNN.
+
+
+
+wire update_weight_ram; // 0: not update; 1: update
+wire [`WEIGHT_WRITE_ADDR_WIDTH*`PARA_KERNEL - 1:0] update_weight_ram_addr;
+wire init_fm_ram_ready; // 0: not ready; 1: ready
+wire init_weight_ram_ready;
+wire [`PARA_X*`PARA_Y*`DATA_WIDTH - 1:0] init_fm_data;
+wire [`WRITE_ADDR_WIDTH - 1:0] write_fm_data_addr;
+wire init_fm_data_done;
+wire [`KERNEL_SIZE_MAX*`KERNEL_SIZE_MAX*`PARA_KERNEL*`DATA_WIDTH - 1:0] weight_data;
+wire [`WEIGHT_WRITE_ADDR_WIDTH*`PARA_KERNEL - 1:0] write_weight_data_addr;
+wire weight_data_done;
+
+DataTransmission DT(
+    .clk(clk),
+    .rst(rst),
+    
+    .init(init),                                                               //init: a waveform generated in FSM which looks like:_____-______
+    
+    .update_weight_ram(update_weight_ram), // 0: not update; 1: update
+    .update_weight_ram_addr(update_weight_ram_addr),
+    
+    .init_fm_ram_ready(init_fm_ram_ready), // 0: not ready; 1: ready
+    .init_weight_ram_ready(init_weight_ram_ready), // 0: not ready; 1: ready
+    
+    .init_fm_data(init_fm_data),
+    .write_fm_data_addr(write_fm_data_addr),
+    .init_fm_data_done(init_fm_data_done),
+    
+    .weight_data(weight_data),
+    .write_weight_data_addr(write_weight_data_addr),
+    .weight_data_done(weight_data_done) // weight data transmission, 0: not ready; 1: ready
+    );
+    
+    
+    
+LayerParaScaleFloat16 LPS(
+	.clk(clk),
+	.rst(rst),
+
+	.layer_type(layer_type), // 0: prepare init feature map and weight data; 1:conv; 2:pool; 3:fc;
+	input [1:0] pre_layer_type,
+
+	input [`LAYER_NUM_WIDTH - 1:0] layer_num,
+
+	// data init and data update
+	.init_fm_data(init_fm_data),
+	.write_fm_data_addr(write_fm_data_addr),
+	.init_fm_data_done(init_fm_data_done), // feature map data transmission, 0: not ready; 1: ready
+
+	.weight_data(weight_data),
+	.write_weight_data_addr(write_weight_data_addr),
+	.weight_data_done(weight_data_done), // weight data transmission, 0: not ready; 1: ready
+
+	// common configuration
+	input [`FM_SIZE_WIDTH - 1:0] fm_size,
+	input [`KERNEL_SIZE_WIDTH - 1:0] fm_depth,
+
+	input [`FM_SIZE_WIDTH - 1:0] fm_size_out, // include padding
+	input [`PADDING_NUM_WIDTH - 1:0] padding_out,
+
+	// conv
+	input [`KERNEL_NUM_WIDTH - 1:0] kernel_num, // fm_depth_out
+	input [`KERNEL_SIZE_WIDTH - 1:0] kernel_size,
+
+	// pool
+	input pool_type, // 0: max pool; 1: avg pool
+	input [`POOL_SIZE_WIDTH - 1:0] pool_win_size, 
+
+	// activation
+	input [1:0] activation, // 0: none; 1: ReLU. current just none or ReLU
+
+	.update_weight_ram(update_weight_ram), // 0: not update; 1: update
+	.update_weight_ram_addr(update_weight_ram_addr),
+
+	.init_fm_ram_ready(init_fm_ram_ready), // 0: not ready; 1: ready
+	.init_weight_ram_ready(init_weight_ram_read), // 0: not ready; 1: ready
+	output reg layer_ready
+    );
 
     
 endmodule
